89604236724606d86468c9bc5916faa1f238dfd3
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-21 00:08:36 +0100

    update def __init__

--- models/base_model.py
+++ models/base_model.py
@@ -9,19 +9,18 @@ class BaseModel:
     attributes/methods for other classes
     """
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
 
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value,
-                            "%Y-%m-%dT%H:%M:%S.%f"))
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-                elif key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+            if key == "created_at" or key == "updated_at":
+                setattr(self, key, datetime.strptime(value,
+                    "%Y-%m-%dT%H:%M:%S.%f"))
+            elif key != "__class__":
+                setattr(self, key, value)
+            
 
     def __str__(self):
         className = self.__class__.__name__
