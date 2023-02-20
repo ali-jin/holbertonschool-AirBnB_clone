f7b74b501f9eb9f2cbe179b22211465fc0695beb
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-21 00:02:25 +0100

    update def __init__

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,14 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         if kwargs:
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+
+                elif key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
