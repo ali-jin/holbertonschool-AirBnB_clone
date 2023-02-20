b79d932300ed8f47bfaf12de41bb51259b691388
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-20 23:26:14 +0100

    update def to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" BaseModel class """
 import uuid
 from datetime import datetime
 
@@ -12,16 +13,18 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    def __str__(self):
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
+
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
         dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
+        dictionary['__class__'] = type(self).__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
 
-    def __str__(self):
-        className = self.__class__.__name__
-        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
+    
