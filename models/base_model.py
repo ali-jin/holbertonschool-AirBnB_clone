8b3396179d7e2060965b6974ca64a4322ad9363d
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-21 11:09:15 +0100

    task 5 : the beginning

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ BaseModel class """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -20,6 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         className = self.__class__.__name__
@@ -27,6 +29,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         dictionary = self.__dict__.copy()
