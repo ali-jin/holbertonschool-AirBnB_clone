09807af9fc0181b1c1d630a44ffd2bfc3ea10c8e
root <root@pc>
2023-02-21 12:31:56 +0100

    add the function reload()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,20 +9,26 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-def all(self):
-    return self.__objects
+    def all(self):
+        return self.__objects
 
-def new(self, obj):
-    key = obj.__class__.__name__ + "." + str(obj.id)
-    self.__objects[key] = obj
+    def new(self, obj):
+        key = obj.__class__.__name__ + "." + str(obj.id)
+        self.__objects[key] = obj
 
-def save(self):
-    new_dict = {}
+    def save(self):
+        new_dict = {}
 
-    for key, value in self.__objects.items():
-        new_dict[key] = value.to_dict()
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
 
-        with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump(new_dict, file)
+            with open(self.__file_path, "w", encoding="utf-8") as file:
+                json.dump(new_dict, file)
 
-def reload(self):
+    def reload(self):
+        try:
+            if path.isfile('__file_path') == True:
+                with open(self.__file_path, "r", encoding="utf-8") as file:
+                    json.loads(file)
+        except FileNotFoundError:
+            pass
