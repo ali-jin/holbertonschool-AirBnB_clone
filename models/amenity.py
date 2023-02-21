a2f20efbcad06aba49c6ae802779488782aaec9a
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-21 23:36:56 +0100

    add documentation

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+Module containing the class Amenity
+"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
+    """
+    Class Amenity inherits from BaseModel
+    """
     name = ""
--- models/review.py
+++ models/review.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""
+Module containing the class Review
+"""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """
+    Class Review inherits from BaseModel
+    """
+
     place_id = ""
     user_id = ""
     text = ""
