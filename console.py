6ade156d3e2f871f41b61db0fa5453030ae218e6
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-21 17:24:49 +0100

    task 7 : add do_create & the beginning of do_show

--- console.py
+++ console.py
@@ -31,6 +31,55 @@ class HBNBCommand(cmd.Cmd):
         """ help for quit """
         print("Quit command to exit the program\n")
 
+    
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file),
+        and prints the id
+        """
+        args = args.split()[0]
+
+        if not args:
+            print("** class name missing **")
+
+        elif args in self.tab:
+            new_instance = eval(args + '()')
+            new_instance.save()
+
+            print(new_instance.id)
+    
+        else:
+            print("** class doesn't exist **")
+    
+    def do_show(self, args):
+        """
+        Prints the string representation of an instance based on
+        the class name and id
+        """
+        args = args.split()
+
+        if not args:
+            print("** class name missing **")
+        
+
+
+    def do_destroy(self, args):
+        """
+        Deletes an instance based on the class name and id
+        """
+
+    def do_all(self, args):
+        """
+        Prints all string representation of all instances based or not
+        on the class name
+        """
+
+
+    def do_update(self, args):
+        """
+        Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file)
+        """
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
