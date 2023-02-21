b4c0ebc491919876a017134160049c01cd480515
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-21 23:35:36 +0100

    add state_id & name & documentation

--- models/city.py
+++ models/city.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Module containing the class City
+"""
 from models.base_model import BaseModel
 
 
-class City(BaseModel)
+class City(BaseModel):
+    """ Define the City class and inherits from BaseModel """
+    state_id = ""
+    name = ""
