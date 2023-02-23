50ec8daaaa9a711e46788122cf480b583ab04b73
root <root@pc>
2023-02-23 14:58:14 +0100

    fix test state_id

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,5 +5,4 @@ from models.city import City
 class test_city(unittest.TestCase):
     def test_city(self):
         self.assertEqual(City.name, "")
-        self.assertEqual(City.state_id, [])
-
+        self.assertEqual(City.state_id, "")
